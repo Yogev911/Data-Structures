22889a072a403ff07bb4d9884a99f47511d0974e
Yogev Heskia <yogev911@gmail.com>
2021-07-28 01:59:05 -0700

    2

--- leetcode.py
+++ leetcode.py
@@ -29,6 +29,23 @@ def twoSum(nums: List[int], target: int) -> List[int]:
             return [h_map[candidate], i]
 
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+@printer
+def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
+    '''
+    2.
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    '''
+    # baby you are cute
+    return ListNode()
+
+
 if __name__ == '__main__':
     generated_list = [random.randint(0, 10) for _ in range(0, random.randint(0, 100))]
     generated_num = random.randint(1, random.randint(2, 10))
