0e29adfb8a77a0c766b55f1bc487d24db49866fe
Yogev Heskia <yogev911@gmail.com>
2021-08-03 11:47:33 +0300

    202

--- leetcode.py
+++ leetcode.py
@@ -364,8 +364,29 @@ def addDigits(num: int) -> int:
     return num
 
 
+def isHappy(n: int) -> bool:
+    """
+    202. Happy Number - Easy
+    Write an algorithm to determine if a number n is happy.
+    A happy number is a number defined by the following process:
+    Starting with any positive integer, replace the number by the sum of the squares of its digits.
+    Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+    Those numbers for which this process ends in 1 are happy.
+    Return true if n is a happy number, and false if not.
+    """
+    h_set = set()
+    while n != 1:
+        if n in h_set:
+            return False
+        digits = [int(x) ** 2 for x in str(n)]
+        h_set.add(n)
+        n = sum(digits)
+    return True
+
+
 if __name__ == '__main__':
-    binaryGap(15)
+    isHappy(9876)
+    # binaryGap(15)
     # isPalindrome(1)
     # myAtoi("21474836460")
     # reverse(1234689)
